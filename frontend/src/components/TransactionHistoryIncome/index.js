7e5611e9e930f6b6f86ca4514824547977207e2a
vnikonov63 <nikonov.va@yandex.ru>
2020-09-21 00:02:19 +0300

    Release add the fix

--- frontend/src/components/TransactionHistoryIncome/index.js
+++ frontend/src/components/TransactionHistoryIncome/index.js
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-function TransactionsHistoryExpense({ id }) {
+function TransactionsHistoryIncome({ id }) {
+  console.log(id);
   const transactions = useSelector(state => state.transactions);
-  const store = useSelector(state => state.categories);
   const transaction = transactions.filter(transaction => {
     return transaction._id === id;
   })[0];
   const [prettyTime, setPrettyTime] = useState('');
-  const [nameFrom, setNameFrom] = useState(null);
   useEffect(() => {
     setPrettyTime(new Date(transaction.time).toLocaleString());
-    setNameFrom(
-      store.filter(category => {
-        return category.id === transaction.from;
-      })[0]
-    );
   }, [transaction]);
 
   return (
@@ -24,11 +18,10 @@ function TransactionsHistoryExpense({ id }) {
         <h2 style={{ margin: '20px' }}>
           ${transaction && transaction.amount}{' '}
         </h2>
-        <h2 style={{ margin: '20px' }}> From {nameFrom && nameFrom.name} </h2>
         <h2 style={{ margin: '20px' }}>At time: {prettyTime && prettyTime}</h2>
       </div>
     </span>
   );
 }
 
-export default TransactionsHistoryExpense;
+export default TransactionsHistoryIncome;
