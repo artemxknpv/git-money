8fd97361716d3c2a0032adcd224e0d6a04d7adaa
vnikonov63 <nikonov.va@yandex.ru>
2020-09-21 19:00:48 +0300

    The display of the transactioons with the time necessary

--- frontend/src/pages/ExpenseList.js
+++ frontend/src/pages/ExpenseList.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import differenceInHours from 'date-fns/differenceInHours';
+import formatDistance from 'date-fns/formatDistance';
 
 import TransactionHistoryExpenses from '../components/TransactionHistoryExpenses';
 
@@ -17,9 +19,41 @@ const ExpenseList = () => {
       return category.id === cat;
     })[0];
   });
-  const transactionsToThisExpense = transactions.filter(transaction => {
+  let transactionsToThisExpense = transactions.filter(transaction => {
     return transaction.to === cat;
   });
+  function sortTime(elementA, elementB) {
+    if (elementA.time < elementB.time) {
+      return 1;
+    } else if (elementA.time > elementB.time) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+  transactionsToThisExpense = transactionsToThisExpense.sort(sortTime);
+  transactionsToThisExpense = transactionsToThisExpense.map(transaction => {
+    return {
+      ...transaction,
+      stringTime:
+        differenceInHours(Date.now(), new Date(transaction.time)) === 0
+          ? 'this hour'
+          : formatDistance(Date.now(), new Date(transaction.time)),
+    };
+  });
+  const objectTime = {};
+  transactionsToThisExpense.map(transaction => {
+    const currentStringTime = transaction.stringTime;
+    if (objectTime[currentStringTime]) {
+      objectTime[currentStringTime].push(transaction);
+    } else {
+      objectTime[currentStringTime] = [transaction];
+    }
+  });
+  console.log(objectTime);
+  // Object.keys(objectTime).forEach(key => {
+  //   console.log(key, objectTime[key]);
+  // });
   return (
     <div>
       <h3>
@@ -31,12 +65,26 @@ const ExpenseList = () => {
           {currentCategory && currentCategory.name}
         </h5>
         {transactionsToThisExpense.length ? (
+          Object.keys(objectTime).map(key => {
+            return (
+              <div>
+                <h2 style={{ color: 'red' }}>{key}</h2>
+                {objectTime[key].map(transaction => {
+                  return <TransactionHistoryExpenses id={transaction._id} />;
+                })}
+              </div>
+            );
+          })
+        ) : (
+          <h2>История ваших покупок в данной категории пуста</h2>
+        )}
+        {/* {transactionsToThisExpense.length ? (
           transactionsToThisExpense.map(element => {
             return <TransactionHistoryExpenses id={element._id} />;
           })
         ) : (
           <h2>История ваших покупок в данной категории пуста</h2>
-        )}
+        )} */}
       </section>
     </div>
   );
