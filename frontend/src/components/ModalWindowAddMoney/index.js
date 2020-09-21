7974605ff68eaf92604ef391519a5ff6747cab78
Pavel <pavtar676@gmail.com>
2020-09-21 15:24:36 +0300

    Modal done for addModal

--- frontend/src/components/ModalWindowAddMoney/index.js
+++ frontend/src/components/ModalWindowAddMoney/index.js
@@ -4,6 +4,29 @@ import closeModalWindow from '../../redux/actions/modalWindow/closeModalWindowAd
 import styles from './ModalWindowAddMoney.module.scss';
 import addMoneyStarted from '../../redux/actions/addMoney/addMoneyStarted';
 import TransactionHistoryIncome from '../../components/TransactionHistoryIncome/index';
+import {motion, AnimatePresence} from 'framer-motion'
+
+const backdrop = {
+  visible: {opacity: 1},
+  hidden: {opacity: 0},
+}
+const modal = {
+  hidden: {
+    x: "-50%",
+    y: "-50%",
+    scale: 0.1,
+    opacity: 0
+  },
+  visible: {
+    x: '-50%',
+    y: "-50%",
+    scale: 1,
+    opacity: 1,
+    transition: { delay: 0.2, duration: 0.3 }
+
+
+  },
+}
 
 const ModalWindowAddMoney = ({ show }) => {
   const [sum, setSum] = useState('');
@@ -15,41 +38,93 @@ const ModalWindowAddMoney = ({ show }) => {
   );
   console.log(thisCategoryList);
   const dispatch = useDispatch();
-  const showHideClassName = show
-    ? `${styles.modal} ${styles.displayBlock}`
-    : `${styles.modal} ${styles.displayNone}`;
+  // const showHideClassName = show
+  //   ? `${styles.modal} ${styles.displayBlock}`
+  //   : `${styles.modal} ${styles.displayNone}`;
   return (
-    <div className={`${showHideClassName}`}>
-      <button onClick={() => dispatch(closeModalWindow())}>x</button>
-      <section className={styles.modalMain}>
-        <h3 className={styles.modalHeader}>Добавить сумму</h3>
-        <p className={styles.modalSubheader}>
-          Указанная сумма будет добавлена к этой категории
-        </p>
-        {thisCategoryList &&
-          thisCategoryList.map(transaction => {
-            return <TransactionHistoryIncome id={transaction._id} />;
-          })}
-        <input
-          type="text"
-          id="sum"
-          placeholder={'1000'}
-          value={sum}
-          onChange={event => setSum(event.target.value)}
-          className={styles.input}
-        />
-        <button
-          className={styles.addButton}
-          onClick={() => {
-            dispatch(addMoneyStarted(userId, id, Number(sum)));
-            dispatch(closeModalWindow());
-          }}
+    <AnimatePresence exitBeforeEnter>
+      {show ?
+        <>
+        <motion.div className={styles.backdrop}
+                    onClick={() => dispatch(closeModalWindow())}
+        variants={backdrop}
+                    initial='hidden'
+                    animate='visible'
         >
-          Добавить
-        </button>
-      </section>
-    </div>
-  );
+        </motion.div>
+          <motion.div className={styles.modalMain}
+          variants={modal}
+                      initial='hidden'
+                      animate='visible'
+          >
+           <h3 className={styles.modalHeader}>Добавить сумму</h3>
+               <p className={styles.modalSubheader}>
+                Указанная сумма будет добавлена к этой категории
+                 </p>
+                {thisCategoryList &&
+                thisCategoryList.map(transaction => {
+                  return <TransactionHistoryIncome id={transaction._id
+                  }
+                  />;
+              })}
+            <input
+              type="text"
+              id="sum"
+              placeholder={'1000'}
+              value={sum}
+              onChange={event => setSum(event.target.value)}
+              className={styles.input}
+            />
+            <button
+              className={styles.addButton}
+              onClick={() => {
+                dispatch(addMoneyStarted(userId, id, Number(sum)));
+                dispatch(closeModalWindow());
+              }}
+            >
+              Добавить
+            </button>
+          </motion.div>
+        </>
+
+        : <></>
+
+      }
+    </AnimatePresence>
+
+);
 };
 
 export default ModalWindowAddMoney;
+
+// <div className={showHideClassName}>
+//   <button onClick={() => dispatch(closeModalWindow())}>x</button>
+//   <section className={styles.modalMain}>
+//     <h3 className={styles.modalHeader}>Добавить сумму</h3>
+//     <p className={styles.modalSubheader}>
+//       Указанная сумма будет добавлена к этой категории
+//     </p>
+//     {thisCategoryList &&
+//     thisCategoryList.map(transaction => {
+//       return <TransactionHistoryIncome id={transaction._id} />;
+//     })}
+//     <input
+//       type="text"
+//       id="sum"
+//       placeholder={'1000'}
+//       value={sum}
+//       onChange={event => setSum(event.target.value)}
+//       className={styles.input}
+//     />
+//     <button
+//       className={styles.addButton}
+//       onClick={() => {
+//         dispatch(addMoneyStarted(userId, id, Number(sum)));
+//         dispatch(closeModalWindow());
+//       }}
+//     >
+//       Добавить
+//     </button>
+//   </section>
+// </div>
+// )
