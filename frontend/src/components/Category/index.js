30663f97a85d27f4d45e1d2d198c4668272c20b6
ArtemK <brtoaster@gmail.com>
2020-09-22 15:27:22 +0300

    usehistory deleted

--- frontend/src/components/Category/index.js
+++ frontend/src/components/Category/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import BagOfGoods from '../../img/BagOfGoods.jsx';
 import WalletFilledIcon from '../../img/WalletFilledIcon.jsx';
