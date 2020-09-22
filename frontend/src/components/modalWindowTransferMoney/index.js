376b9bf85c764f85082c5dc15d809d5c8f251bd7
ateworld9 <vahrameev.work@gmail.com>
2020-09-22 17:57:11 +0300

    add: some validation

--- frontend/src/components/modalWindowTransferMoney/index.js
+++ frontend/src/components/modalWindowTransferMoney/index.js
@@ -4,6 +4,7 @@ import closeModalWindow from '../../redux/actions/modalWindow/closeModalWindowTr
 import styles from './ModalWindowTransferMoney.module.scss';
 import transferMoneyStarted from '../../redux/actions/transferMoney/transferMoneyStarted';
 import { motion, AnimatePresence } from 'framer-motion';
+import { set } from 'date-fns';
 
 const backdrop = {
   visible: { opacity: 1 },
@@ -27,8 +28,10 @@ const modal = {
 
 const ModalWindowAddMoney = ({ show }) => {
   const [sum, setSum] = useState('');
+  const [error, setError] = useState('');
   const userId = useSelector(state => state.user._id);
   let nameFrom = '';
+  let currentNumberFrom = 0;
   let nameTo = '';
   const categories = useSelector(state => state.categories);
   const idTo = useSelector(state => state.isTransferMoneyModal.idTo);
@@ -36,19 +39,37 @@ const ModalWindowAddMoney = ({ show }) => {
 
   if (idTo && idFrom) {
     nameFrom = categories.filter(category => category.id === idFrom)[0].name;
+    currentNumberFrom = categories.filter(category => category.id === idFrom)[0]
+      .currentNumber;
     nameTo = categories.filter(category => category.id === idTo)[0].name || '';
   }
   const dispatch = useDispatch();
   // const showHideClassName = show
   //   ? `${styles.modal} ${styles.displayBlock}`
   //   : `${styles.modal} ${styles.displayNone}`;
+
+  const handleTransferMoney = () => {
+    if (currentNumberFrom - Number(sum) > 0) {
+      dispatch(transferMoneyStarted(userId, idTo, idFrom, Number(sum)));
+      setSum('');
+      setError('');
+      dispatch(closeModalWindow());
+    } else {
+      setError('Недостаточно средств для перевода');
+    }
+  };
+
   return (
     <AnimatePresence exitBeforeEnter>
       {show ? (
         <>
           <motion.div
             className={styles.backdrop}
-            onClick={() => dispatch(closeModalWindow())}
+            onClick={() => {
+              dispatch(closeModalWindow());
+              setError('');
+              setSum('');
+            }}
             variants={backdrop}
             initial="hidden"
             animate="visible"
@@ -72,14 +93,11 @@ const ModalWindowAddMoney = ({ show }) => {
               onChange={event => setSum(event.target.value)}
               className={styles.input}
             />
+            {error && <p className={styles.modalSubheader}>{error}</p>}
             <button
               className={styles.addButton}
               onClick={() => {
-                dispatch(
-                  transferMoneyStarted(userId, idTo, idFrom, Number(sum))
-                );
-                setSum('');
-                dispatch(closeModalWindow());
+                handleTransferMoney();
               }}
             >
               Добавить
