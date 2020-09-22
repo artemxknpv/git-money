660fd5071987437161dbe946981810e39f8d9ba1
ateworld9 <vahrameev.work@gmail.com>
2020-09-22 13:01:39 +0300

    release add keys in incomes and expenses

--- frontend/src/components/Income/index.js
+++ frontend/src/components/Income/index.js
@@ -17,7 +17,7 @@ const Income = () => {
       return acc + store.currentNumber;
     }, 0);
     dispatch(setTotalMoney(totalMoney));
-  }, [dispatch, incomes]);
+  }, []);
 
   return (
     <>
@@ -34,7 +34,7 @@ const Income = () => {
                 {incomes &&
                   incomes.map((income, index) => {
                     return (
-                      <div>
+                      <div key={index}>
                         <Draggable
                           key={income.id}
                           draggableId={income.id}
