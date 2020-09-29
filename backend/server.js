ad3c7e20bdffbdbb11e458088b22d6dce01dd518
ArtemK <brtoaster@gmail.com>
2020-09-29 18:29:14 +0300

    delete package lock

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,9 @@ import session from "express-session";
 import fileStore from "session-file-store";
 import mongoose from "mongoose";
 import { modelUser } from "./models/user.js";
-// import passport from "passport";
 import categoryRouter from "./routes/categories.js";
 import authenticateRouter from "./routes/authenticate.js";
-// import flash from "express-flash";
-// Passport Config
-// import {
-//   // googleAuthentication,
-//   localAuthentication,
-// } from "./config/passport.config.js";
-// localAuthentication(
-//   passport,
-//   login => modelUser.findOne({ login }),
-//   id => modelUser.findById(id)
-// );
-// googleAuthentication(passport);
+import path from "path";
 
 mongoose.connect(
   "mongodb+srv://user_me:123ER123@cluster0.opbgv.mongodb.net/final?retryWrites=true&w=majority",
@@ -26,7 +14,6 @@ mongoose.connect(
 
 const app = express();
 const FileStore = fileStore(session);
-// app.use(flash());
 app.use(express.json());
 
 app.use(
@@ -44,14 +31,6 @@ app.use(
   })
 );
 
-// // Passport middleware
-// app.use(passport.initialize()); //TODO
-// app.use(passport.session());
-
-// app.get("/", (req, res) => {
-//   res.send("Hello");
-// });
-
 // create a new user
 app.put("/", async (req, res) => {
   await modelUser.createDefaultUser();
@@ -60,6 +39,11 @@ app.put("/", async (req, res) => {
 
 app.use("/auth", authenticateRouter);
 app.use(categoryRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("..frontend", "build", "index.html"));
+});
+
 app.use((err, req, res, next) => {
   console.log(">>>>", err);
 });
